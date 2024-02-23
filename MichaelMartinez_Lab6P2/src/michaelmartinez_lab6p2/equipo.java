5be65c6751348e7e085c440228d03ecb985361ed
michael-martinez14 <michaelmartinez2083@unitec.edu>
2024-02-23 14:21:28 -0600

    c1

---
+++ MichaelMartinez_Lab6P2/src/michaelmartinez_lab6p2/equipo.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package michaelmartinez_lab6p2;
+
+/**
+ *
+ * @author Michael Martínez
+ */
+public class equipo {
+    public String nombre,pais,ciudad,estadio;
+
+    public equipo(String nombre, String pais, String ciudad, String estadio) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.ciudad = ciudad;
+        this.estadio = estadio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre: " + nombre + ", pais: " + pais;
+    }
+    
+    
+}
---
+++ MichaelMartinez_Lab6P2/src/michaelmartinez_lab6p2/jugador.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package michaelmartinez_lab6p2;
+
+/**
+ *
+ * @author Michael Martínez
+ */
+public class jugador {
+    public String nombre,posicion, edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre;
+    }
+    
+    
+    
+}
